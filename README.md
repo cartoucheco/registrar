dcd65efa373d61bfc7fe7bdb17ca209176b544ca
liubenben <qieda@163.com>
2020-05-07 10:01:42 +0800

    new luxe registrar contracts with alterable resolver

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# registrar
-Registrar for Cartouche-managed ENS TLDs.
+# About luxe-registrar contract
+
+Here is the updated version of the LUXE domain registrar contract.
+
+## Target
+
+1. Maintain the original basic framework and authorization mechanism of LUXE domain system, and complete the support for multiple chains of LUXE domain in ENS system.
+2. After the contract is deployed, the migration of LUXE top-level domain to the new ENS registry can be completed.
+
+## Major Upgrade
+
+1. The registrar has been upgraded to integrate a new resolver which is a implementation of the latest ENS resolver interface. And in the alterable-resolver version described later, the default resolver is alterable.
+2. The default resolver has been upgraded, and now the resolver has implemented the latest ENS resolver interface, which supports multi-chain address, content hash, text and other records.
+
+## The difference between the two versions
+
+Considering that LUXE is integrated into ENS system in a custom way, I wonder if there is any new functional requirements of the resolver in the future. Therefore, I have written two versions of the LUXE registry contract, please choose to use according to the situation. The two versions are located in the constant-resolver and alterable-resolver branches.
+
+### constant-resolver version
+
+This version USES a fixed default resolver. It create a new resolver when the registrar contract is deployed and use them as the default resolver for users registering the LUXE domain. This resolver will remain the default resolver for users registering the LUXE domain unless the entire LUXE registrar contract is redeployed.
+
+### alterable-resolver version
+
+This version USES a alterable default resolver. It create a new resolver when the registrar contract is deployed and use them as the default resolver for users registering the LUXE domain. Unlike the constant-resolver version, the default resolver is deployed separately in another contract and set in the registrar, and the default resolver can be modified by the "owner" role of the registrar. Therefore, if you are faced with the need to change the functionality of the default resolver in the future, you can not deploy the entire LUXE registrar contract, just deploy the resolver contract separately and change the default resolver used by the registrar to a new address.
+
+## Other
+
+1. To migrate the LUXE top-level domain to the new ENS registry, the latest address of the ENS registry, i.e. `0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e`, must be used at the deployment of this contract.
+2. The compiler version I used is 0.5.16+commit.9c3226ce
--- contracts/OwnedRegistrar.sol
+++ contracts/OwnedRegistrar.sol
@@ -1,9 +1,9 @@
-pragma solidity ^0.4.20;
+pragma solidity >=0.5.0 <0.7.0;
 pragma experimental ABIEncoderV2;
 
 import "@ensdomains/ens/contracts/ENS.sol";
 import "./RBAC.sol";
-import "./OwnerResolver.sol";
+import "./Resolver.sol";
 
 /**
  * OwnedRegistrar implements an ENS registrar that accepts registrations by a
@@ -12,10 +12,13 @@ import "./OwnerResolver.sol";
  * removed by an account with the "authoriser" role.
  *
  * An audit of this code is available here: https://hackmd.io/s/SJcPchO57
+ *
+ * The default resolver for users is alterable in this version, but only the
+ * "owner" can reset it.
  */
 contract OwnedRegistrar is RBAC {
     ENS public ens;
-    OwnerResolver public resolver;
+    address public resolver;
     mapping(uint=>mapping(address=>bool)) public registrars; // Maps IANA IDs to authorised accounts
     mapping(bytes32=>uint) public nonces; // Maps namehashes to domain nonces
 
@@ -23,18 +26,19 @@ contract OwnedRegistrar is RBAC {
     event RegistrarRemoved(uint id, address registrar);
     event Associate(bytes32 indexed node, bytes32 indexed subnode, address indexed owner);
     event Disassociate(bytes32 indexed node, bytes32 indexed subnode);
+    event ResolverReset(address newResolver);
 
-    constructor(ENS _ens) public {
+    constructor(ENS _ens, address _resolver) public {
         ens = _ens;
-        resolver = new OwnerResolver(_ens);
+        resolver = _resolver;
         _addRole(msg.sender, "owner");
     }
 
-    function addRole(address addr, string role) external onlyRole("owner") {
+    function addRole(address addr, string calldata role) external onlyRole("owner") {
         _addRole(addr, role);
     }
 
-    function removeRole(address addr, string role) external onlyRole("owner") {
+    function removeRole(address addr, string calldata role) external onlyRole("owner") {
         // Don't allow owners to remove themselves
         require(keccak256(abi.encode(role)) != keccak256(abi.encode("owner")) || msg.sender != addr);
         _removeRole(addr, role);
@@ -50,6 +54,11 @@ contract OwnedRegistrar is RBAC {
         emit RegistrarRemoved(id, registrar);
     }
 
+    function resetResolver(address newResolver) public onlyRole("owner") {
+        resolver = newResolver;
+        emit ResolverReset(newResolver);
+    }
+
     function associateWithSig(bytes32 node, bytes32 label, address owner, uint nonce, uint registrarId, bytes32 r, bytes32 s, uint8 v) public onlyRole("transactor") {
         bytes32 subnode = keccak256(abi.encode(node, label));
         require(nonce == nonces[subnode]);
@@ -60,19 +69,21 @@ contract OwnedRegistrar is RBAC {
         require(registrars[registrarId][registrar]);
 
         ens.setSubnodeOwner(node, label, address(this));
-        if(owner == 0) {
-            ens.setResolver(subnode, 0);
+        if(owner == address(0)) {
+            ens.setResolver(subnode, address(0));
         } else {
             ens.setResolver(subnode, resolver);
+            Resolver(resolver).setAddr(subnode, owner);
         }
         ens.setOwner(subnode, owner);
 
         emit Associate(node, label, owner);
     }
 
-    function multicall(bytes[] calls) public {
-        for(uint i = 0; i < calls.length; i++) {
-            require(address(this).delegatecall(calls[i]));
+    function multicall(bytes[] memory calls) public returns (bool) {
+        for (uint i = 0; i < calls.length; i++) {
+            (bool success, ) = address(this).delegatecall(calls[i]);
+            require(success, "One or more of the transactions failed!");
         }
     }
 }
